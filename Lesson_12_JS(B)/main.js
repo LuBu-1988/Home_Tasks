51158c5eca66d58da5e50edc56d591046705fad9
LuBu-1988 <nonamelubu@gmail.com>
2020-06-20 10:45:18 +0300

    lesson_12_JS_2

--- Lesson_12_JS(B)/main.js
+++ Lesson_12_JS(B)/main.js
@@ -1,23 +1,25 @@
 function familiya () {
-    var myFam = document.getElementById('yFam').value;
-    var myName = document.getElementById('yName').value;
-    var mySN = document.getElementById('sName').value;
-    var myOld = parseInt(document.getElementById('old').value);
-    var myGen = document.getElementById('gender').value;
-    var pen;
+    var myFam = document.getElementById('yFam').value,
+        myName = document.getElementById('yName').value,
+        mySN = document.getElementById('sName').value,
+        myOld = parseInt(document.getElementById('old').value),
+        myGen = document.getElementById('gender').value,
+        pen = '';
 
     var ageDays = myOld * 365;
     var myAge = myOld + 5;
 
-    if (myGen=='Мужской') {
-        if (myOld>=60)
+    if (myGen=='Мужской' && myOld>=60 || myGen=='Женский' && myOld>=55) {
         pen += 'да';
-    else if (myGen=='Женский')
-        if (myOld>=55)
-        pen += 'да';
-    else 
+    }    
+    else {
         pen += 'нет';
     }
+
+    
+    // Тернарная операция
+    // pen = (myGen=='Мужской' && myOld>=60 || myGen=='Женский' && myOld>=55)?'да':'нет';
+    
 
     alert('Ваши ФИО: ' + myFam + ' ' + myName + ' ' + mySN + '\n' + 'Ваш возраст в годах: ' + myOld
      + '\n' + 'Ваш возраст в днях: ' + ageDays + '\n' + 'Через 5 лет вам будет: ' + myAge + '\n' 
